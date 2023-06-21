1385f661c78b5b161f43abc16473a798ab4bbf29
Oleg Karpov <oleg1karpov@gmail.com>
2023-06-21 16:32:40 +0400

    feature: send photoGroup via http link

--- src/main/java/com/karpov/astrobot/services/SendPhotoService.java
+++ src/main/java/com/karpov/astrobot/services/SendPhotoService.java
@@ -1,12 +1,22 @@
 package com.karpov.astrobot.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.karpov.astrobot.bot.AstroBot;
 import com.karpov.astrobot.config.BotConfig;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.media.InputMediaPhoto;
 
+import java.util.HashSet;
+import java.util.Set;
+
+//org.telegram.telegrambots for some reason doesn't have this methods
 @Service
 public class SendPhotoService {
 
@@ -18,14 +28,46 @@ public class SendPhotoService {
 		this.botConfig = botConfig;
 	}
 
-	public void sendPhotoFromHTTP(String chatId, String http) {
-		restTemplate.exchange("https://api.telegram.org/bot{botToken}/sendPhoto?chat_id={chatId}&photo={photoLink}",
-				HttpMethod.POST,
-				HttpEntity.EMPTY,
-				String.class,
-				botConfig.getBotToken(),
-				chatId,
-				http
-		);
+	public HttpStatusCode sendPhotoFromHTTP(String chatId, String http) {
+		try {
+			ResponseEntity<Message> responseEntity = restTemplate.getForEntity("https://api.telegram.org/bot{botToken}/sendPhoto?chat_id={chatId}&photo={photoLink}",
+					Message.class,
+					botConfig.getBotToken(),
+					chatId,
+					http);
+			return responseEntity.getStatusCode();
+		} catch (HttpClientErrorException e) {
+			e.printStackTrace();
+			return e.getStatusCode();
+		}
+
+	}
+
+	// set should be from 2 to 10
+	public HttpStatusCode sendPhotoGroupFromHTTP(String chatId, Set<String> set) {
+		try {
+
+			Set<InputMediaPhoto> setInputMediaPhotoGroup = new HashSet<>();
+
+			for (String http : set) {
+				InputMediaPhoto inputMediaPhoto = new InputMediaPhoto();
+				inputMediaPhoto.setMedia(http);
+				setInputMediaPhotoGroup.add(inputMediaPhoto);
+			}
+
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.enable(SerializationFeature.INDENT_OUTPUT);
+			String json = mapper.writeValueAsString(setInputMediaPhotoGroup);
+
+			ResponseEntity<Message> responseEntity = restTemplate.getForEntity("https://api.telegram.org/bot{botToken}/sendMediaGroup?chat_id={chatId}&media={json}",
+					Message.class,
+					botConfig.getBotToken(),
+					chatId,
+					json);
+			return responseEntity.getStatusCode();
+		} catch (HttpClientErrorException | JsonProcessingException e) {
+			e.printStackTrace();
+			return HttpStatusCode.valueOf(404);
+		}
 	}
 }
