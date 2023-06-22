4c990ea73d8f4b193b66ca34bf28915dcdf62b49
Oleg Karpov <oleg1karpov@gmail.com>
2023-06-22 11:43:36 +0400

    feature: Aurora forecast service

---
+++ src/main/java/com/karpov/astrobot/services/AuroraForecastService.java
@@ -0,0 +1,43 @@
+package com.karpov.astrobot.services;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+@Service
+public class AuroraForecastService {
+
+	public String get3DayAuroraForecast() {
+		try {
+			URL url = new URL("https://services.swpc.noaa.gov/text/3-day-forecast.txt");
+			Scanner scanner = new Scanner(url.openStream());
+			StringBuilder stringBuilder = new StringBuilder();
+			for (int i = 0; i < 22; i++) {
+				stringBuilder.append(scanner.nextLine()).append(System.lineSeparator());
+			}
+			scanner.close();
+			return stringBuilder.toString();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return "Service unavailable";
+		}
+	}
+
+	public String get27DayAuroraForecast() {
+		try {
+			URL url = new URL("https://services.swpc.noaa.gov/text/27-day-outlook.txt");
+			Scanner scanner = new Scanner(url.openStream());
+			StringBuilder stringBuilder = new StringBuilder();
+			while(scanner.hasNextLine()) {
+				stringBuilder.append(scanner.nextLine()).append(System.lineSeparator());
+			}
+			scanner.close();
+			return stringBuilder.toString();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return "Service unavailable";
+		}
+	}
+}
