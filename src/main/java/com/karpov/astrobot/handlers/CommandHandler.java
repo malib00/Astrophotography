7c4a2684b5d3e8244544bc30cfc388488cc20824
Oleg Karpov <oleg1karpov@gmail.com>
2023-06-22 11:56:30 +0400

    feature: command handlers: help, settings

--- src/main/java/com/karpov/astrobot/handlers/CommandHandler.java
+++ src/main/java/com/karpov/astrobot/handlers/CommandHandler.java
@@ -1,35 +1,47 @@
 package com.karpov.astrobot.handlers;
 
 import com.karpov.astrobot.keyboards.MainMenuKeyboard;
+import com.karpov.astrobot.keyboards.SettingsKeyboard;
 import com.karpov.astrobot.services.RegisterChatService;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.Arrays;
 
 @Component
 public class CommandHandler {
 
 	private final RegisterChatService registerChatService;
 	private final MainMenuKeyboard mainMenuKeyboard;
+	private final SettingsKeyboard settingsKeyboard;
 
-	public CommandHandler(RegisterChatService registerChatService, MainMenuKeyboard mainMenuKeyboard) {
+	public CommandHandler(RegisterChatService registerChatService, MainMenuKeyboard mainMenuKeyboard, SettingsKeyboard settingsKeyboard) {
 		this.registerChatService = registerChatService;
 		this.mainMenuKeyboard = mainMenuKeyboard;
+		this.settingsKeyboard = settingsKeyboard;
 	}
 
 	public SendMessage handleCommand(Update update, String command) {
+		SendMessage sendMessage = new SendMessage();
+		sendMessage.setChatId(update.getMessage().getChatId().toString());
+		MessageEntity messageEntity = new MessageEntity("code", 0, 23);
+		sendMessage.setEntities(Arrays.asList(messageEntity));
+
 		switch (command) {
 			case ("/start"):
 				registerChatService.registerChatOrUpdateState(update);
-				SendMessage sendMessage = new SendMessage();
-				sendMessage.setChatId(update.getMessage().getChatId().toString());
-				sendMessage.setText("Menu");
+				sendMessage.setText("Astrophotography Helper\n\nMain Menu");
 				sendMessage.setReplyMarkup(mainMenuKeyboard.getMainMenuInlineKeyboard());
 				return sendMessage;
 			case ("/help"):
-				return new SendMessage(update.getMessage().getChatId().toString(), "Some help text here... gonna be soon");
+				sendMessage.setText("Astrophotography Helper\n\nUse /start to open Main Menu");
+				return sendMessage;
 			case ("/settings"):
-				return new SendMessage(update.getMessage().getChatId().toString(), "Settings is not implemented yet");
+				sendMessage.setText("Astrophotography Helper\n\nSettings");
+				sendMessage.setReplyMarkup(settingsKeyboard.getSettingsInlineKeyboard());
+				return sendMessage;
 			default:
 				return new SendMessage(update.getMessage().getChatId().toString(), command + " - command not recognized");
 		}
